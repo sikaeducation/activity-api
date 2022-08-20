872cdcb399ed2396203c9823d026c808453a45e8
Kyle Coberly <kyle.coberly@gmail.com>
2022-08-20 14:56:59 -0600

    Lint test files

--- tests/database-connection.ts
+++ tests/database-connection.ts
@@ -1,11 +1,11 @@
-import {MongoClient} from "mongodb";
+import { MongoClient } from "mongodb";
 
-const url = process.env.DATABASE_URL
-if (!url) throw new Error("DATABASE_URL must be defined")
+const url = process.env.DATABASE_URL;
+if (!url) throw new Error("DATABASE_URL must be defined");
 
-const client = new MongoClient(url)
+const client = new MongoClient(url);
 
 export async function getDatabase() {
-  await client.connect()
-  return client.db("activityService")
+  await client.connect();
+  return client.db("activityService");
 }
--- tests/features/support/steps/general.ts
+++ tests/features/support/steps/general.ts
@@ -1,23 +1,20 @@
-import {When, Then} from "@cucumber/cucumber"
-import request from "supertest"
-import {expect} from "expect"
+import { When, Then } from "@cucumber/cucumber";
+import request from "supertest";
+import { expect } from "expect";
 
 When("I make a request to the index route", async function () {
-  return this.request = request(this.app)
-    .get("/")
-    .expect(200)
-})
+  return (this.request = request(this.app).get("/").expect(200));
+});
 
 Then("I get description of the API", async function () {
-  const response = await this.request
-    .expect("Content-Type", /json/)
+  const response = await this.request.expect("Content-Type", /json/);
 
   expect(response.body).toMatchObject({
-    description: expect.stringMatching(/.*/)
-  })
-})
+    description: expect.stringMatching(/.*/),
+  });
+});
 
-type HTTPMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE"
+type HTTPMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
 
 const httpMethods = {
   GET: "get",
@@ -25,10 +22,13 @@ const httpMethods = {
   PUT: "put",
   PATCH: "patch",
   DELETE: "delete",
-} as const
+} as const;
 
-When("I make a {string} request to the {string} endpoint", async function (method: HTTPMethod, endpoint: string) {
-  console.log(endpoint)
-  const normalizedMethod = httpMethods[method]
-  this.request = request(this.app)[normalizedMethod](endpoint)
-});
+When(
+  "I make a {string} request to the {string} endpoint",
+  async function (method: HTTPMethod, endpoint: string) {
+    console.log(endpoint);
+    const normalizedMethod = httpMethods[method];
+    this.request = request(this.app)[normalizedMethod](endpoint);
+  }
+);
