abd90d86a9bc2ab0bfaf664ead953633737d9fe3
Kyle Coberly <kyle.coberly@gmail.com>
2022-08-20 14:56:59 -0600

    Lint test files

--- tests/features/support/world.ts
+++ tests/features/support/world.ts
@@ -1,9 +1,11 @@
-import {setWorldConstructor, World} from "@cucumber/cucumber"
-import app from "../../api/src/app"
-import type {Application} from "express";
-import type {SuperTest} from "supertest";
-import type {SuperAgentRequest} from "superagent";
+import { setWorldConstructor, World } from "@cucumber/cucumber";
+import app from "../../api/src/app";
+import type { Application } from "express";
+import type { SuperTest } from "supertest";
+import type { SuperAgentRequest } from "superagent";
 
-setWorldConstructor(function (this: World & {app: Application, request: SuperTest<SuperAgentRequest>}) {
-  this.app = app
+setWorldConstructor(function (
+  this: World & { app: Application; request: SuperTest<SuperAgentRequest> }
+) {
+  this.app = app;
 });
