c2adbf969ae3f619b8d8cc4b894dc44031adc84e
Kyle Coberly <kyle.coberly@gmail.com>
2022-11-09 13:13:06 -0700

    Update express port

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App is listening on ${PORT}`);
 });
