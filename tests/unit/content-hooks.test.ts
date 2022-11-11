e06543de5ffd21dc88a1ec7035895e60b4051ef5
Kyle Coberly <kyle.coberly@gmail.com>
2022-11-11 15:20:55 -0700

    Add unit tests

---
+++ tests/unit/content-hooks.test.ts
@@ -0,0 +1,53 @@
+import { HookContext } from "@feathersjs/feathers";
+import { expect, jest, test } from "@jest/globals";
+import { getContent, getAllContent } from "../../src/hooks/content";
+
+jest.mock("../../src/services/posts", () => ({
+  getPost: jest.fn(() => "# Some Markdown"),
+}));
+
+test("#getContent adds content to single responses", async () => {
+  const context = getContent({
+    result: {
+      _type: "Article",
+      post_slug: "mongo-intro",
+    },
+  } as HookContext);
+  expect(context.result.content).toEqual("# Some Markdown");
+});
+
+test("#getAllContent adds content to list responses", async () => {
+  const context = getAllContent({
+    result: [
+      {
+        _type: "Article",
+        post_slug: "mongo-intro",
+      },
+      {
+        _type: "Guide",
+        post_slug: "mongo-intermediate",
+      },
+      {
+        _type: "SomeOtherType",
+        post_slug: "mongo-advanced",
+      },
+    ],
+  } as HookContext);
+
+  expect(context.result).toMatchObject([
+    {
+      _type: "Article",
+      post_slug: "mongo-intro",
+      content: "# Some Markdown",
+    },
+    {
+      _type: "Guide",
+      post_slug: "mongo-intermediate",
+      content: "# Some Markdown",
+    },
+    {
+      _type: "SomeOtherType",
+      post_slug: "mongo-advanced",
+    },
+  ]);
+});
