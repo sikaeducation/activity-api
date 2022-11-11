3e1ad3190be7de331e824a8b16e3f705153850c9
Kyle Coberly <kyle.coberly@gmail.com>
2022-11-11 15:31:03 -0700

    Add regen posts route

---
+++ src/routes/regenerate-posts.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { populatePosts } from "../services/posts";
+import { verifyWebHook } from "../integrations/github";
+
+import type { Request, Response } from "express";
+
+const router = express.Router();
+
+router.post(
+  "/regenerate-posts",
+  async (request: Request, response: Response) => {
+    const signature = Buffer.from(
+      request.get("X-Hub-Signature-256") || "",
+      "utf8"
+    );
+    const isValid =
+      process.env.NODE_ENV !== "production" ||
+      verifyWebHook(request.body, signature);
+    if (isValid) {
+      console.log("Populating posts...");
+      await populatePosts();
+      response.status(200).send();
+    } else {
+      response.status(401).send();
+    }
+  }
+);
+
+export default router;
