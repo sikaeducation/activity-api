c349382c1bd443d19f6c6ca54c1850d43fca2347
Kyle Coberly <kyle.coberly@gmail.com>
2022-12-01 15:28:43 -0700

    Reorganize middleware

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import express from "@feathersjs/express";
+import cors from "cors";
+import morgan from "morgan";
+import { populatePosts } from "./services/posts";
+import { connect as connectToDatabase } from "./database-connection";
+import type { Application } from "@feathersjs/express";
+
+export function attachMiddleware(app: Application) {
+  connectToDatabase();
+  app.configure(express.rest());
+  app.use(cors());
+  app.use(express.json());
+  if (process.env.NODE_ENV !== "test") app.use(morgan("tiny"));
+  if (process.env.NODE_ENV === "production")
+    !(async () => await populatePosts())();
+}
