77066f8c11cccc68a521ab4685ea5cb194b59f64
Kyle Coberly <kyle.coberly@gmail.com>
2022-12-01 15:24:27 -0700

    Reorganize routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,31 @@
-import { Router } from "@feathersjs/express";
-import { Request, Response } from "express";
+import indexRoutes from "./home";
+import regeneratePostsRoutes from "./regenerate-posts";
+import service from "feathers-mongoose";
+import {
+  Activity,
+  Article,
+  Guide,
+  Exercise,
+  VocabList,
+  Lesson,
+  Video,
+  Vocab,
+  Question,
+} from "../models";
+import express from "@feathersjs/express";
+import type { Application } from "@feathersjs/express";
 
-const router = Router();
-
-router.get("/", (request: Request, response: Response) => {
-  response.json({
-    description: "Sika's API for activities (articles, quizzes, lessons, etc.)",
-  });
-});
-
-export default router;
+export function attachRoutes(app: Application) {
+  app.use(
+    "/activities",
+    service({
+      Model: Activity,
+      discriminators: [Article, Guide, Exercise, VocabList, Lesson, Video],
+    } as any) // mongoose-feathers is missing the discriminators key in this type
+  );
+  app.use("/vocabs", service({ Model: Vocab, lean: true }));
+  app.use("/questions", service({ Model: Question, lean: true }));
+  app.use("/", indexRoutes);
+  app.use(regeneratePostsRoutes);
+  app.use(express.errorHandler());
+}
