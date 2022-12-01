9b2e74e7d1984b9fcf195f66c808743cc9b7f9df
Kyle Coberly <kyle.coberly@gmail.com>
2022-11-30 17:00:59 -0700

    Add token fixture

--- src/routes/regenerate-posts.ts
+++ src/routes/regenerate-posts.ts
@@ -10,7 +10,6 @@ router.post(
   "/regenerate-posts",
   async (request: Request, response: Response) => {
     const rawSignature = request.get("X-Hub-Signature-256") || "";
-    console.log({ body: JSON.stringify(request.body), rawSignature });
     const signature = Buffer.from(rawSignature, "utf8");
     const isValid =
       ["production", "test"].includes(process.env.NODE_ENV || "") &&
---
+++ tests/unit/github-token-validation-fixture.ts
@@ -0,0 +1,235 @@
+export const signature =
+  "sha256=d4f47b99863cd56ae43a78bd2dca7af3efbd6a6ab9e5dcb6a57609fdde452268";
+export const body = {
+  ref: "refs/heads/master",
+  before: "45c51544d6f71630c1eaf20ca0a717f728e46ae1",
+  after: "4f2f01d43e4927c570befd915a4f98916baecaf9",
+  repository: {
+    id: 447353375,
+    node_id: "R_kgDOGqoSHw",
+    name: "posts",
+    full_name: "sikaeducation/posts",
+    private: true,
+    owner: {
+      name: "sikaeducation",
+      email: "admin@sikaeducation.com",
+      login: "sikaeducation",
+      id: 73668455,
+      node_id: "MDEyOk9yZ2FuaXphdGlvbjczNjY4NDU1",
+      avatar_url: "https://avatars.githubusercontent.com/u/73668455?v=4",
+      gravatar_id: "",
+      url: "https://api.github.com/users/sikaeducation",
+      html_url: "https://github.com/sikaeducation",
+      followers_url: "https://api.github.com/users/sikaeducation/followers",
+      following_url:
+        "https://api.github.com/users/sikaeducation/following{/other_user}",
+      gists_url: "https://api.github.com/users/sikaeducation/gists{/gist_id}",
+      starred_url:
+        "https://api.github.com/users/sikaeducation/starred{/owner}{/repo}",
+      subscriptions_url:
+        "https://api.github.com/users/sikaeducation/subscriptions",
+      organizations_url: "https://api.github.com/users/sikaeducation/orgs",
+      repos_url: "https://api.github.com/users/sikaeducation/repos",
+      events_url: "https://api.github.com/users/sikaeducation/events{/privacy}",
+      received_events_url:
+        "https://api.github.com/users/sikaeducation/received_events",
+      type: "Organization",
+      site_admin: false,
+    },
+    html_url: "https://github.com/sikaeducation/posts",
+    description: null,
+    fork: false,
+    url: "https://github.com/sikaeducation/posts",
+    forks_url: "https://api.github.com/repos/sikaeducation/posts/forks",
+    keys_url: "https://api.github.com/repos/sikaeducation/posts/keys{/key_id}",
+    collaborators_url:
+      "https://api.github.com/repos/sikaeducation/posts/collaborators{/collaborator}",
+    teams_url: "https://api.github.com/repos/sikaeducation/posts/teams",
+    hooks_url: "https://api.github.com/repos/sikaeducation/posts/hooks",
+    issue_events_url:
+      "https://api.github.com/repos/sikaeducation/posts/issues/events{/number}",
+    events_url: "https://api.github.com/repos/sikaeducation/posts/events",
+    assignees_url:
+      "https://api.github.com/repos/sikaeducation/posts/assignees{/user}",
+    branches_url:
+      "https://api.github.com/repos/sikaeducation/posts/branches{/branch}",
+    tags_url: "https://api.github.com/repos/sikaeducation/posts/tags",
+    blobs_url:
+      "https://api.github.com/repos/sikaeducation/posts/git/blobs{/sha}",
+    git_tags_url:
+      "https://api.github.com/repos/sikaeducation/posts/git/tags{/sha}",
+    git_refs_url:
+      "https://api.github.com/repos/sikaeducation/posts/git/refs{/sha}",
+    trees_url:
+      "https://api.github.com/repos/sikaeducation/posts/git/trees{/sha}",
+    statuses_url:
+      "https://api.github.com/repos/sikaeducation/posts/statuses/{sha}",
+    languages_url: "https://api.github.com/repos/sikaeducation/posts/languages",
+    stargazers_url:
+      "https://api.github.com/repos/sikaeducation/posts/stargazers",
+    contributors_url:
+      "https://api.github.com/repos/sikaeducation/posts/contributors",
+    subscribers_url:
+      "https://api.github.com/repos/sikaeducation/posts/subscribers",
+    subscription_url:
+      "https://api.github.com/repos/sikaeducation/posts/subscription",
+    commits_url:
+      "https://api.github.com/repos/sikaeducation/posts/commits{/sha}",
+    git_commits_url:
+      "https://api.github.com/repos/sikaeducation/posts/git/commits{/sha}",
+    comments_url:
+      "https://api.github.com/repos/sikaeducation/posts/comments{/number}",
+    issue_comment_url:
+      "https://api.github.com/repos/sikaeducation/posts/issues/comments{/number}",
+    contents_url:
+      "https://api.github.com/repos/sikaeducation/posts/contents/{+path}",
+    compare_url:
+      "https://api.github.com/repos/sikaeducation/posts/compare/{base}...{head}",
+    merges_url: "https://api.github.com/repos/sikaeducation/posts/merges",
+    archive_url:
+      "https://api.github.com/repos/sikaeducation/posts/{archive_format}{/ref}",
+    downloads_url: "https://api.github.com/repos/sikaeducation/posts/downloads",
+    issues_url:
+      "https://api.github.com/repos/sikaeducation/posts/issues{/number}",
+    pulls_url:
+      "https://api.github.com/repos/sikaeducation/posts/pulls{/number}",
+    milestones_url:
+      "https://api.github.com/repos/sikaeducation/posts/milestones{/number}",
+    notifications_url:
+      "https://api.github.com/repos/sikaeducation/posts/notifications{?since,all,participating}",
+    labels_url:
+      "https://api.github.com/repos/sikaeducation/posts/labels{/name}",
+    releases_url:
+      "https://api.github.com/repos/sikaeducation/posts/releases{/id}",
+    deployments_url:
+      "https://api.github.com/repos/sikaeducation/posts/deployments",
+    created_at: 1642017531,
+    updated_at: "2022-01-13T18:57:49Z",
+    pushed_at: 1669852448,
+    git_url: "git://github.com/sikaeducation/posts.git",
+    ssh_url: "git@github.com:sikaeducation/posts.git",
+    clone_url: "https://github.com/sikaeducation/posts.git",
+    svn_url: "https://github.com/sikaeducation/posts",
+    homepage: null,
+    size: 36606,
+    stargazers_count: 0,
+    watchers_count: 0,
+    language: "HTML",
+    has_issues: true,
+    has_projects: true,
+    has_downloads: true,
+    has_wiki: true,
+    has_pages: false,
+    has_discussions: false,
+    forks_count: 0,
+    mirror_url: null,
+    archived: false,
+    disabled: false,
+    open_issues_count: 0,
+    license: null,
+    allow_forking: false,
+    is_template: false,
+    web_commit_signoff_required: false,
+    topics: [],
+    visibility: "private",
+    forks: 0,
+    open_issues: 0,
+    watchers: 0,
+    default_branch: "master",
+    stargazers: 0,
+    master_branch: "master",
+    organization: "sikaeducation",
+  },
+  pusher: {
+    name: "kylecoberly",
+    email: "kyle.coberly@gmail.com",
+  },
+  organization: {
+    login: "sikaeducation",
+    id: 73668455,
+    node_id: "MDEyOk9yZ2FuaXphdGlvbjczNjY4NDU1",
+    url: "https://api.github.com/orgs/sikaeducation",
+    repos_url: "https://api.github.com/orgs/sikaeducation/repos",
+    events_url: "https://api.github.com/orgs/sikaeducation/events",
+    hooks_url: "https://api.github.com/orgs/sikaeducation/hooks",
+    issues_url: "https://api.github.com/orgs/sikaeducation/issues",
+    members_url: "https://api.github.com/orgs/sikaeducation/members{/member}",
+    public_members_url:
+      "https://api.github.com/orgs/sikaeducation/public_members{/member}",
+    avatar_url: "https://avatars.githubusercontent.com/u/73668455?v=4",
+    description: "Engineering education",
+  },
+  sender: {
+    login: "kylecoberly",
+    id: 1958931,
+    node_id: "MDQ6VXNlcjE5NTg5MzE=",
+    avatar_url: "https://avatars.githubusercontent.com/u/1958931?v=4",
+    gravatar_id: "",
+    url: "https://api.github.com/users/kylecoberly",
+    html_url: "https://github.com/kylecoberly",
+    followers_url: "https://api.github.com/users/kylecoberly/followers",
+    following_url:
+      "https://api.github.com/users/kylecoberly/following{/other_user}",
+    gists_url: "https://api.github.com/users/kylecoberly/gists{/gist_id}",
+    starred_url:
+      "https://api.github.com/users/kylecoberly/starred{/owner}{/repo}",
+    subscriptions_url: "https://api.github.com/users/kylecoberly/subscriptions",
+    organizations_url: "https://api.github.com/users/kylecoberly/orgs",
+    repos_url: "https://api.github.com/users/kylecoberly/repos",
+    events_url: "https://api.github.com/users/kylecoberly/events{/privacy}",
+    received_events_url:
+      "https://api.github.com/users/kylecoberly/received_events",
+    type: "User",
+    site_admin: false,
+  },
+  created: false,
+  deleted: false,
+  forced: false,
+  base_ref: null,
+  compare:
+    "https://github.com/sikaeducation/posts/compare/45c51544d6f7...4f2f01d43e49",
+  commits: [
+    {
+      id: "4f2f01d43e4927c570befd915a4f98916baecaf9",
+      tree_id: "ac33db2afa8d3387c8cb6efa40712e265a1ea91c",
+      distinct: true,
+      message: "Update agile",
+      timestamp: "2022-11-30T16:54:05-07:00",
+      url: "https://github.com/sikaeducation/posts/commit/4f2f01d43e4927c570befd915a4f98916baecaf9",
+      author: {
+        name: "Kyle Coberly",
+        email: "kyle.coberly@gmail.com",
+        username: "kylecoberly",
+      },
+      committer: {
+        name: "Kyle Coberly",
+        email: "kyle.coberly@gmail.com",
+        username: "kylecoberly",
+      },
+      added: [],
+      removed: [],
+      modified: ["agile/README.md"],
+    },
+  ],
+  head_commit: {
+    id: "4f2f01d43e4927c570befd915a4f98916baecaf9",
+    tree_id: "ac33db2afa8d3387c8cb6efa40712e265a1ea91c",
+    distinct: true,
+    message: "Update agile",
+    timestamp: "2022-11-30T16:54:05-07:00",
+    url: "https://github.com/sikaeducation/posts/commit/4f2f01d43e4927c570befd915a4f98916baecaf9",
+    author: {
+      name: "Kyle Coberly",
+      email: "kyle.coberly@gmail.com",
+      username: "kylecoberly",
+    },
+    committer: {
+      name: "Kyle Coberly",
+      email: "kyle.coberly@gmail.com",
+      username: "kylecoberly",
+    },
+    added: [],
+    removed: [],
+    modified: ["agile/README.md"],
+  },
+};
