771212bbdcf5d6369f89036ebe93e33c40e3ef97
Kyle Coberly <kyle.coberly@gmail.com>
2022-12-01 16:47:19 -0700

    Add auth middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,8 +1,9 @@
 import express from "@feathersjs/express";
 import cors from "cors";
 import morgan from "morgan";
-import { populatePosts } from "./services/posts";
-import { connect as connectToDatabase } from "./database-connection";
+import { authenticate } from "./auth0";
+import { populatePosts } from "../services/posts";
+import { connect as connectToDatabase } from "../database-connection";
 import type { Application } from "@feathersjs/express";
 
 export function attachMiddleware(app: Application) {
@@ -11,6 +12,8 @@ export function attachMiddleware(app: Application) {
   app.use(cors());
   app.use(express.json());
   if (process.env.NODE_ENV !== "test") app.use(morgan("tiny"));
-  if (process.env.NODE_ENV === "production")
+  if (process.env.NODE_ENV === "production") {
+    app.use(authenticate);
     !(async () => await populatePosts())();
+  }
 }
