5e40f69d8bd3ae523baee879ea6106934ff4a56b
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-14 00:31:15 -0600

    ESLint works

--- src/services/github.ts
+++ src/services/github.ts
@@ -1,62 +1,64 @@
 import { Octokit } from "octokit";
 import AdmZip, { IZipEntry } from "adm-zip";
 import axios from "axios";
-import { flow, mapValues, keyBy } from "lodash/fp";
+import { mapValues, keyBy } from "lodash/fp";
 import crypto from "crypto";
 
 async function getArchiveUrl() {
-  const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN,
-  });
-  return octokit.rest.repos
-    .downloadZipballArchive({
-      owner: "sikaeducation",
-      repo: "posts",
-      ref: "master",
-    })
-    .then((response) => response.url);
+	const octokit = new Octokit({
+		auth: process.env.GITHUB_TOKEN,
+	});
+	return octokit.rest.repos
+		.downloadZipballArchive({
+			owner: "sikaeducation",
+			repo: "posts",
+			ref: "master",
+		})
+		.then((response) => response.url);
 }
 
 async function getFiles(url: string) {
-  return axios.get(url, { responseType: "arraybuffer" }).then((response) => {
-    const zip = new AdmZip(response.data);
-    return zip.getEntries();
-  });
+	return axios.get(url, { responseType: "arraybuffer" }).then((response) => {
+		const zip = new AdmZip(response.data);
+		return zip.getEntries();
+	});
 }
 
 function entryToFile(entry: IZipEntry) {
-  return {
-    name: entry.entryName.split("/")[1],
-    content: entry.getData().toString("utf8"),
-  };
+	return {
+		name: entry.entryName.split("/")[1],
+		content: entry.getData().toString("utf8"),
+	};
 }
 
 function processFiles(entries: IZipEntry[]) {
-  const files = entries
-    .filter((entry) => entry.name === "README.md")
-    .map(entryToFile);
-  return flow([keyBy("name"), mapValues("content")])(files);
+	const files = entries
+		.filter((entry) => entry.name === "README.md")
+		.map(entryToFile);
+
+	const byName = keyBy("name")(files);
+	return mapValues("content")(files);
 }
 
 export async function getPostContent() {
-  return getArchiveUrl()
-    .then(getFiles)
-    .then(processFiles)
-    .catch((error) => {
-      console.error(error.message);
-      return {};
-    });
+	return getArchiveUrl()
+		.then(getFiles)
+		.then(processFiles)
+		.catch((error as Error) => {
+			console.error(error.message);
+			return {};
+		});
 }
 
 export function verifyWebHook(body: unknown, rawSignature: string) {
-  const signature = Buffer.from(rawSignature, "utf8");
-  const GITHUB_WEBHOOK_TOKEN = process.env.GITHUB_WEBHOOK_TOKEN || "";
+	const signature = Buffer.from(rawSignature, "utf8");
+	const GITHUB_WEBHOOK_TOKEN = process.env.GITHUB_WEBHOOK_TOKEN || "";
 
-  const hmac = crypto.createHmac("sha256", GITHUB_WEBHOOK_TOKEN);
-  const digest = Buffer.from(
-    `sha256=${hmac.update(JSON.stringify(body)).digest("hex")}`,
-    "utf8"
-  );
+	const hmac = crypto.createHmac("sha256", GITHUB_WEBHOOK_TOKEN);
+	const digest = Buffer.from(
+		`sha256=${hmac.update(JSON.stringify(body)).digest("hex")}`,
+		"utf8"
+	);
 
-  return crypto.timingSafeEqual(signature, digest);
+	return crypto.timingSafeEqual(signature, digest);
 }
