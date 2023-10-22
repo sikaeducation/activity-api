65ead91cfaddf70ef5de2bd9dbe529c68443c5dd
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-22 04:20:50 -0600

    --wip-- [skip ci]

--- src/routes/regenerate-posts.ts
+++ src/routes/regenerate-posts.ts
@@ -8,12 +8,9 @@ export default function RegeneratePostsRoute(
   response: Response,
 ) {
   const rawSignature = request.get("X-Hub-Signature-256") || "";
-  console.log("rawSignature", rawSignature);
-  console.log("NODE_ENV", process.env.NODE_ENV);
   const isValid =
     ["production", "test"].includes(process.env.NODE_ENV || "") &&
     verifyWebHook(request.body, rawSignature);
-  console.log("isValid", isValid);
   if (isValid) {
     console.log("valid");
     populatePosts().then(() => response.status(200).send());
