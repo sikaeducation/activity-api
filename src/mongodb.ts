4b2fbee4d771ff5d59e61340c5ec749994c96dc5
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-25 04:24:26 -0600

    Update environment vars

--- src/mongodb.ts
+++ src/mongodb.ts
@@ -3,17 +3,17 @@ import type { Db } from "mongodb";
 import type { Application } from "./declarations";
 
 declare module "./declarations" {
-  interface Configuration {
-    mongodbClient: Promise<Db>;
-    mongoDatabase: string;
-  }
+	interface Configuration {
+		mongodbClient: Promise<Db>;
+		mongoDatabase: string;
+	}
 }
 
 export const mongodb = (app: Application) => {
-  const connection = app.get("mongodb") as string;
-  const mongoClient = MongoClient.connect(connection).then((client) =>
-    client.db(app.get("mongoDatabase")),
-  );
+	const connection = app.get("mongodb") as string;
+	const mongoClient = MongoClient.connect(connection).then((client) =>
+		client.db(app.get("mongoDatabase")),
+	);
 
-  app.set("mongodbClient", mongoClient);
+	app.set("mongodbClient", mongoClient);
 };
