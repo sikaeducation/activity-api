fd3629196cf166c2072a0678934443682c7f57af
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-26 01:36:40 -0600

    --wip-- [skip ci]

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import { feathers } from "@feathersjs/feathers";
 import feathersExpress, {
-  rest,
-  json,
-  cors,
-  notFound,
-  errorHandler,
+	rest,
+	json,
+	cors,
+	notFound,
+	errorHandler,
 } from "@feathersjs/express";
 import configuration from "@feathersjs/configuration";
 import socketio from "@feathersjs/socketio";
@@ -31,39 +31,39 @@ app.use(json());
 
 // Configure services and real-time functionality
 app.configure(swagger.customMethodsHandler).configure(
-  swagger({
-    specs: {
-      info: {
-        title: "Activity API",
-        version: "1.0.0",
-      },
-      schemes: ["http", "https"],
-      components: {
-        securitySchemes: {
-          BearerAuth: {
-            type: "http",
-            scheme: "bearer",
-          },
-        },
-      },
-      security: [{ BearerAuth: [] }],
-    },
-    prefix: "",
-    ignore: {
-      paths: [/authentication/i],
-    },
-    ui: swagger.swaggerUI({
-      docsPath: "/docs",
-    }),
-  }),
+	swagger({
+		specs: {
+			info: {
+				title: "Activity API",
+				version: "1.0.0",
+			},
+			schemes: ["http", "https"],
+			components: {
+				securitySchemes: {
+					BearerAuth: {
+						type: "http",
+						scheme: "bearer",
+					},
+				},
+			},
+			security: [{ BearerAuth: [] }],
+		},
+		prefix: "",
+		ignore: {
+			paths: [/authentication/i],
+		},
+		ui: swagger.swaggerUI({
+			docsPath: "/docs",
+		}),
+	}),
 );
 app.configure(rest());
 app.configure(
-  socketio({
-    cors: {
-      origin: app.get("origins"),
-    },
-  }),
+	socketio({
+		cors: {
+			origin: app.get("origins"),
+		},
+	}),
 );
 app.configure(mongodb);
 app.configure(authentication);
@@ -76,24 +76,24 @@ app.post("/regenerate-posts", verifyWebhookMiddleware, regeneratePostsRoute);
 // Configure a middleware for 404s and the error handler
 app.use(notFound());
 if (process.env.NODE_ENV !== "test") {
-  app.use(errorHandler({ logger }));
+	app.use(errorHandler({ logger }));
 } else {
-  app.use(errorHandler({ logger: false }));
+	app.use(errorHandler({ logger: false }));
 }
 
 // Register hooks that run on all service methods
 app.hooks({
-  around: {
-    all: [logError],
-  },
-  before: {},
-  after: {},
-  error: {},
+	around: {
+		all: [logError],
+	},
+	before: {},
+	after: {},
+	error: {},
 });
 // Register application setup and teardown hooks here
 app.hooks({
-  setup: [populatePosts],
-  teardown: [],
+	setup: [populatePosts],
+	teardown: [],
 });
 
 export { app };
