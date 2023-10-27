98050158f559b6216fee6505537d903315f56b5e
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-26 18:00:59 -0600

    Debugging

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import { feathers } from "@feathersjs/feathers";
 import feathersExpress, {
-	rest,
-	json,
-	cors,
-	notFound,
-	errorHandler,
+  rest,
+  json,
+  cors,
+  notFound,
+  errorHandler,
 } from "@feathersjs/express";
 import configuration from "@feathersjs/configuration";
 import socketio from "@feathersjs/socketio";
@@ -20,50 +20,66 @@ import { services } from "./services/index";
 import { channels } from "./channels";
 import { regeneratePostsRoute } from "./routes/regenerate-posts";
 import { verifyWebhookMiddleware } from "./middleware/verify-webhook";
+import expressWinston from "express-winston";
 import { populatePosts } from "./post-cache";
+import winston from "winston";
 
 const app: Application = feathersExpress(feathers());
 
 // Load app configuration
 app.configure(configuration(configurationValidator));
+if (process.env.NODE_ENV === "dev") {
+  app.use(
+    expressWinston.logger({
+      transports: [new winston.transports.Console()],
+      meta: false,
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.printf((info) => `${info.message}`),
+      ),
+      msg: "{{res.statusCode}} - {{req.path}}",
+      colorize: true,
+    }),
+  );
+}
 app.use(cors());
 app.use(json());
 
 // Configure services and real-time functionality
 app.configure(swagger.customMethodsHandler).configure(
-	swagger({
-		specs: {
-			info: {
-				title: "Activity API",
-				version: "1.0.0",
-			},
-			schemes: ["http", "https"],
-			components: {
-				securitySchemes: {
-					BearerAuth: {
-						type: "http",
-						scheme: "bearer",
-					},
-				},
-			},
-			security: [{ BearerAuth: [] }],
-		},
-		prefix: "",
-		ignore: {
-			paths: [/authentication/i],
-		},
-		ui: swagger.swaggerUI({
-			docsPath: "/docs",
-		}),
-	}),
+  swagger({
+    specs: {
+      info: {
+        title: "Activity API",
+        version: "1.0.0",
+      },
+      schemes: ["http", "https"],
+      components: {
+        securitySchemes: {
+          BearerAuth: {
+            type: "http",
+            scheme: "bearer",
+          },
+        },
+      },
+      security: [{ BearerAuth: [] }],
+    },
+    prefix: "",
+    ignore: {
+      paths: [/authentication/i],
+    },
+    ui: swagger.swaggerUI({
+      docsPath: "/docs",
+    }),
+  }),
 );
 app.configure(rest());
 app.configure(
-	socketio({
-		cors: {
-			origin: app.get("origins"),
-		},
-	}),
+  socketio({
+    cors: {
+      origin: app.get("origins"),
+    },
+  }),
 );
 app.configure(mongodb);
 app.configure(authentication);
@@ -76,24 +92,26 @@ app.post("/regenerate-posts", verifyWebhookMiddleware, regeneratePostsRoute);
 // Configure a middleware for 404s and the error handler
 app.use(notFound());
 if (process.env.NODE_ENV !== "test") {
-	app.use(errorHandler({ logger }));
+  app.use(errorHandler({ logger }));
 } else {
-	app.use(errorHandler({ logger: false }));
+  app.use(errorHandler({ logger: false }));
 }
 
 // Register hooks that run on all service methods
 app.hooks({
-	around: {
-		all: [logError],
-	},
-	before: {},
-	after: {},
-	error: {},
+  around: {
+    all: [logError],
+  },
+  before: {},
+  after: {},
+  error: {},
 });
 // Register application setup and teardown hooks here
-app.hooks({
-	setup: [populatePosts],
-	teardown: [],
-});
+if (process.env.NODE_ENV === "production") {
+  app.hooks({
+    setup: [populatePosts],
+    teardown: [],
+  });
+}
 
 export { app };
