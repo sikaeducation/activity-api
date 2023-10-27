10278730741d4cff3b609bf1f19b4c48f999839b
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-27 14:15:39 -0600

    Cleanup

--- environment.d.ts
+++ environment.d.ts
@@ -1,17 +1,12 @@
-import type { Application } from "@/declarations";
-
 declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      PORT: string;
-      WEBHOOK_TOKEN: string;
-      REPO_TOKEN: string;
-      AUTH_KEY_URL: string;
-      DATABASE_URL: string;
-    }
-  }
-}
-
-declare module "#/src/app" {
-  export function app(): Application;
+	namespace NodeJS {
+		interface ProcessEnv {
+			PORT: string;
+			WEBHOOK_TOKEN: string;
+			REPO_TOKEN: string;
+			AUTH_KEY_URL: string;
+			DATABASE_URL: string;
+			SENTRY_URL: string;
+		}
+	}
 }
