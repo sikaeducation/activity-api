aa47dca13515d098ceec0a3b0861f9401fab093a
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-27 14:10:44 -0600

    Update deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,15 @@ import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => ({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, mode === "production" ? "./dist" : "./src"),
-      $: path.resolve(__dirname, "./test-helpers"),
-    },
-  },
-
-  test: {
-    include: ["./**/*.test.ts"],
-    setupFiles: ["./test-helpers/setup-tests.ts"],
-    mockReset: true,
-  },
-
-  build: {
-    sourcemap: true,
-  },
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, mode === "production" ? "./dist" : "./src"),
+			$: path.resolve(__dirname, "./test-helpers"),
+		},
+	},
+	test: {
+		include: ["./**/*.test.ts"],
+		setupFiles: ["./test-helpers/setup-tests.ts"],
+		mockReset: true,
+	},
 }));
